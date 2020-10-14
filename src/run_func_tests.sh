3b7ac2e9fd27eb6afa52af1a964d6fbbc6e65a14
tony_kero <antoine.karcher.id@gmail.com>
2020-10-14 16:26:35 +0200

    Updated script

--- src/run_func_tests.sh
+++ src/run_func_tests.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 for filename in func/tests/*.fun; do
-    echo "=====compile======"
+    echo "==============="
     script=$(basename "$filename")
     script="${script%.*}"
     ./run_func.sh "$script"
-    echo "=====execute======"
-    script="${filename%.*}"
-    ./_build/impc/impc.native "${script}.imp" &&
-    java -jar ./impc/Mars4_5.jar nc "${script}.asm"
 done
