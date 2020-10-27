f19e84b86ff313b8254d5f742a8d291e89a5cb49
tony_kero <antoine.karcher.id@gmail.com>
2020-10-27 18:09:27 +0100

    Updated scripts

--- src/run_func.sh
+++ src/run_func.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "$1 Output:"
-./_build/func/func.native ./func/tests/$1.fun && cat ./func/tests/$1.imp &&
+./_build/func/func.native ./func/tests/$1.fun && #cat ./func/tests/$1.imp &&
 
 ./_build/impc/impc.native ./func/tests/$1.imp &&
 java -jar ./impc/Mars4_5.jar nc ./func/tests/$1.asm
--- src/run_mmlc.sh
+++ src/run_mmlc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "$1 Output:"
-./_build/mmlc/mmlc.native ./mmlc/tests/$1.mml && cat ./mmlc/tests/$1.fun &&
-./_build/func/func.native ./mmlc/tests/$1.fun && cat ./mmlc/tests/$1.imp &&
+./_build/mmlc/mmlc.native ./mmlc/tests/$1.mml && #cat ./mmlc/tests/$1.fun &&
+./_build/func/func.native ./mmlc/tests/$1.fun && #cat ./mmlc/tests/$1.imp &&
 ./_build/impc/impc.native ./mmlc/tests/$1.imp &&
 java -jar ./impc/Mars4_5.jar nc ./mmlc/tests/$1.asm
